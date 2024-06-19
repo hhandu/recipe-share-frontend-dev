ffc1ca32e0dc744467c31166c8a9d326dbb077b3
Anidut0566 Dut <anikdutta167@gmail.com>
2024-06-19 18:57:03 +0300

    login page format

--- src/Pages/login.tsx
+++ src/Pages/login.tsx
@@ -1,5 +1,22 @@
 
 
+import {React} from 'react';
+import * as z from 'zod';
+import { Button } from '@/components/ui/button';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+  } from "@/components/ui/form";
+  import { Input } from "@/components/ui/input";
+  import { Link } from 'react-router-dom';
+  
 const login = () => {
     return (
         <div>
