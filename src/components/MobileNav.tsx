f5329e439c193403cace475bf9efe58f8886a6b0
Anidut0566 Dut <anikdutta167@gmail.com>
2024-06-22 08:54:40 +0300

    update the mobile view

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,9 +1,8 @@
-// this section is the mobile view of the navbar . Hamburger icon is imported from lucide-react. 
-
 import { Menu } from "lucide-react";
 import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "./ui/sheet";
 import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
+import { Link } from "react-router-dom";
 
 const MobileNav = () => {
     return(
@@ -13,21 +12,17 @@ const MobileNav = () => {
             </SheetTrigger>
             <SheetContent className="space-y-3">
                 <SheetTitle>
-                    <span> Welcome to <span className="text-orange-500">Recipe</span><span className="text-blue-500">Share</span></span>
+                    <span>Welcome to <span className="text-orange-500">Recipe</span><span className="text-blue-500">Share</span></span>
                 </SheetTitle>
                 <Separator />
-                
                 <SheetDescription className="flex">
-                    <Button className="flex-1 font-bold bg-orange-500">Log in</Button>
-
-
+                    <Link to="/login" className="flex-1">
+                        <Button className="w-full font-bold bg-orange-500 text-white">Login</Button>
+                    </Link>
                 </SheetDescription>
             </SheetContent>
-
         </Sheet>
-
     )
-
 };
 
 export default MobileNav;
