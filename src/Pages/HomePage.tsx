4cadaf98b6abaadf97e62c875ae7a4d33830e5d0
Anidut0566 Dut <anikdutta167@gmail.com>
2024-06-26 09:16:16 +0300

    add your own recipe button added

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,21 +1,32 @@
+import { Button } from '@/components/ui/button';
 import pic4 from '../assets/pic 4.jpg'
+import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
+    const navigate = useNavigate();
+
+    const handleButtonClick = () => {
+        navigate('/login');
+    };
     return (
         <div className="className=flex flex-col bg-slate-200 px-16">
             <div className="grid md:grid-cols-2">
                 <img className='max-h-[600px] p-3 rounded-lg' src={pic4} />
                 <div className="flex flex-col items-center justify-center gap-4 text-center">
                     <span className="font-bold text-4xl tracking-tighter">
-                    Save it with visuals
+                        Save your own recipe
                     </span>
                     <span className='text-lg'>
-                    No matter what you save in <span className='text-orange-500' >Recipe</span><span className='text-blue-500'>Share</span> recipe instruction,  or food photos  everything is visual, meaning folders and items have images and titles. Don’t like the image, change it or drag and drop or upload your own photo. Whether at the grocery store or cooking, <span className='text-orange-500'>Recipe</span><span className='text-blue-500'>Share</span> helps you find what you are looking for faster with visuals.
+                        No matter what you save in <span className='text-orange-500' >Recipe</span><span className='text-blue-500'>Share</span> recipe instruction,  or food ingredient  everything is visible to other, meaning in the recipe section you can see your own recipe. Don’t like the recipe, check some other recipe, <span className='text-orange-500'>Recipe</span><span className='text-blue-500'>Share</span> will make your cooking easy.
                     </span>
+                    <h4 className='text-lg font-bold'> Do you want to add your own recipe? </h4>
+                    <Button className='bg-orange-500 font-medium' onClick={handleButtonClick}>
+                        Add Your Own Recipe
+                    </Button>
                 </div>
             </div>
         </div>
-            
+
     );
 };
 
